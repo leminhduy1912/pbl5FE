627c3aafa7050b782ce08cc4f556caf35db9c6d9
leminhduy1912 <minhduyle081003@gmail.com>
2024-02-29 11:23:49 +0700

    feat:change register to confirm email

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,34 +1,61 @@
-import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import Header from "../../components/HomePage/Header";
-import { Toaster, toast } from "sonner";
+import { useState } from "react";
 import axios from "axios";
-// import { useNavigate } from "react-router-dom";
-
 import Loading from "../../components/Loading/Loading";
+import { Toaster, toast } from "sonner";
+import { useNavigate } from "react-router-dom";
 const Register = () => {
-  // const navigate = useNavigate();
+  const [action, setAction] = useState("email");
   const [emailSignUp, setEmailSignUp] = useState("");
-  const [passwordSignUp, setPasswordSignUp] = useState("");
-  const [confirmPasswordSignUp, setConfirmPasswordSignUp] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-
-  const handleSignUpProcess = (e) => {
+  const [verifyCode, setVerifyCode] = useState("");
+  const [verificationCode, setVerificationCode] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const navigate = useNavigate();
+  const handleCheckEmailExist = () => {
+    let formData = new FormData();
+    formData.append("email", emailSignUp);
+    setIsLoading(true);
+    axios
+      .post(`${import.meta.env.VITE_API_BE_KEY}/api/v1/auth/email`, formData)
+      .then((response) => {
+        if (response.data.meta.status_code == 200) {
+          console.log("success");
+          setAction("verify");
+          setIsLoading(false);
+          setVerifyCode(response.data.data.result.verifyCode);
+          console.log(response.data.data.result.verifyCode);
+        }
+      })
+      .catch((error) => {
+        if ((error.response.status = 409)) {
+          // Xử lý phản hồi từ máy chủ
+          setIsLoading(false);
+          toast.error("Email has already exist");
+        } else if (error.request) {
+          // Xử lý khi không có phản hồi từ máy chủ
+          console.log(error.request);
+        } else {
+          // Xử lý khi gặp lỗi khác
+          console.log("Error", error.message);
+        }
+      });
+  };
+  const handleSignUp = (e) => {
     e.preventDefault();
 
-    if (
-      confirmPasswordSignUp == "" ||
-      passwordSignUp == "" ||
-      emailSignUp == ""
-    ) {
+    if (confirmPassword == "" || password == "") {
       toast.error("Empty infor");
     } else {
-      if (confirmPasswordSignUp != passwordSignUp) {
+      if (confirmPassword != password) {
         toast.error("Confirm password do not match");
       } else {
         setIsLoading(true);
         let formData = new FormData();
         formData.append("email", emailSignUp);
-        formData.append("password", passwordSignUp);
+        formData.append("password", password);
         axios
           .post(
             `${import.meta.env.VITE_API_BE_KEY}/api/v1/auth/register`,
@@ -41,30 +68,11 @@ const Register = () => {
               console.log("success");
               setIsLoading(false);
               toast.error("Email already in use");
-              // navigate("/");
-              // dispatch(
-              //   login({
-              //     // Pass payload as an object
-              //     id: response.data.data.id,
-              //     token: response.data.data.ACCESS_TOKEN,
-              //     roleCode: response.data.data.role,
-              //     isLogin: true,
-              //   })
-              // );
             }
             if (response.data.meta.status_code == 200) {
               setIsLoading(false);
               toast.success("Sign up success");
-              // navigate("/");
-              // dispatch(
-              //   login({
-              //     // Pass payload as an object
-              //     id: response.data.data.id,
-              //     token: response.data.data.ACCESS_TOKEN,
-              //     roleCode: response.data.data.role,
-              //     isLogin: true,
-              //   })
-              // );
+              navigate("/");
             }
           })
           .catch((error) => {
@@ -76,112 +84,173 @@ const Register = () => {
       }
     }
   };
+  console.log(verifyCode);
+  const handleInputChange = (index, value) => {
+    const updatedCode =
+      verificationCode.substring(0, index) +
+      value +
+      verificationCode.substring(index + 1);
+    setVerificationCode(updatedCode);
+  };
+  const handleCheckMatchVerifyCode = () => {
+    if (verificationCode !== verifyCode) {
+      toast.error("The confirmation code does not match");
+    } else {
+      setAction("register");
+    }
+  };
+  console.log("check", verificationCode);
   return (
     <>
-      {isLoading && <Loading />}
-      <Toaster position="top-center" closeButton richColors />
       <Header />
-      <div className="antialiased bg-white w-full h-full">
-        <div className="max-w-lg mx-auto my-10 bg-white p-8 rounded-xl shadow shadow-slate-300">
-          <h1 className="text-4xl font-medium">Register</h1>
-          <p className="text-slate-500">Hi, Welcome back 👋</p>
-
-          <div className="my-5">
-            <button className="w-full text-center py-3 my-3 border flex space-x-2 items-center justify-center border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150">
-              <img
-                src="https://www.svgrepo.com/show/355037/google.svg"
-                className="w-6 h-6"
-                alt=""
-              />{" "}
-              <span>Register with Google</span>
-            </button>
-          </div>
-          <form action="" className="my-10">
-            <div className="flex flex-col space-y-5">
-              <label htmlFor="email">
-                <p className="font-medium text-slate-700 pb-2">Email address</p>
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow"
-                  placeholder="Enter email address"
-                  onChange={(e) => setEmailSignUp(e.target.value)}
-                />
-              </label>
-              <label htmlFor="password">
-                <p className="font-medium text-slate-700 pb-2">Password</p>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow"
-                  placeholder="Enter your password"
-                  onChange={(e) => setPasswordSignUp(e.target.value)}
-                />
-              </label>
-              <label htmlFor="password">
-                <p className="font-medium text-slate-700 pb-2">
-                  Confirm Password
-                </p>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow"
-                  placeholder="Confirm Password"
-                  onChange={(e) => setConfirmPasswordSignUp(e.target.value)}
-                />
-              </label>
-              <div className="flex flex-row justify-between"></div>
-              <button
-                className="w-full py-3 font-medium text-white bg-indigo-600 hover:bg-indigo-500 rounded-lg border-indigo-500 hover:shadow inline-flex space-x-2 items-center justify-center"
-                onClick={handleSignUpProcess}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
-                  />
-                </svg>
-                <span>Register</span>
-              </button>
-              <p className="text-center">
-                Already have an account?{" "}
-                <a
-                  href="/login"
-                  className="text-indigo-600 font-medium inline-flex space-x-1 items-center"
-                >
-                  <span>Log in now </span>
-                  <span>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-4 w-4"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-                      />
-                    </svg>
-                  </span>
-                </a>
+      <Toaster position="top-center" closeButton richColors />
+      {isLoading && <Loading />}
+      <div className="bg-gray-100">
+        {action == "email" && (
+          <div className="min-h-screen flex items-center justify-center">
+            <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
+              <h1 className="text-xl font-semibold mb-4">Register</h1>
+              <p className="text-gray-600 mb-6">
+                Change the email address you want associated with your account.
               </p>
+              <div className="mb-4">
+                <input
+                  type="email"
+                  placeholder="you@example.com"
+                  className="email-input w-full px-4 py-2 border rounded-lg text-gray-700 focus:border-blue-500"
+                  onChange={(e) => {
+                    setEmailSignUp(e.target.value);
+                  }}
+                />
+              </div>
+              <button
+                className="w-full bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 focus:outline-none"
+                onClick={() => {
+                  handleCheckEmailExist();
+                }}
+              >
+                Next
+              </button>
             </div>
-          </form>
-        </div>
+          </div>
+        )}
+        {action == "verify" && (
+          <AnimatePresence>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-12"
+            >
+              <div className="relative bg-white px-6 pt-10 pb-9 shadow-xl mx-auto w-full max-w-lg rounded-2xl">
+                <div className="mx-auto flex w-full max-w-md flex-col space-y-16">
+                  <div className="flex flex-col items-center justify-center text-center space-y-2">
+                    <div className="font-semibold text-3xl">
+                      <p>Email Verification</p>
+                    </div>
+                    <div className="flex flex-row text-sm font-medium text-gray-400">
+                      <p>
+                        We have sent a code to your email{" "}
+                        <span className="font-bold text-black">
+                          {emailSignUp}
+                        </span>{" "}
+                      </p>
+                    </div>
+                  </div>
+
+                  <div>
+                    <div>
+                      <div className="flex flex-col space-y-16">
+                        <div className="flex flex-row items-center justify-between gap-3 mx-auto w-full max-w-xs">
+                          {Array.from({ length: 4 }).map((_, index) => (
+                            <div className="w-16 h-16 " key={index}>
+                              <input
+                                className="w-full h-full text-black flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border border-gray-200 text-lg bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700"
+                                type="password"
+                                maxLength={1}
+                                value={verificationCode[index] || ""}
+                                onChange={(e) =>
+                                  handleInputChange(index, e.target.value)
+                                }
+                              />
+                            </div>
+                          ))}
+                        </div>
+
+                        <div className="flex flex-col space-y-5">
+                          <div>
+                            <button
+                              onClick={handleCheckMatchVerifyCode}
+                              className="flex flex-row items-center justify-center text-center w-full border rounded-xl outline-none py-5 bg-blue-700 border-none text-white text-sm shadow-sm"
+                            >
+                              Verify
+                            </button>
+                          </div>
+
+                          <div className="flex flex-row items-center justify-center text-center text-sm font-medium space-x-1 text-gray-500">
+                            <p>Didn't recieve code?</p>{" "}
+                            <a
+                              className="flex flex-row items-center text-blue-600"
+                              href="http://"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              Resend
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </motion.div>
+          </AnimatePresence>
+        )}
+        {action == "register" && (
+          <AnimatePresence>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className="min-h-screen flex items-center justify-center"
+            >
+              <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
+                <h1 className="text-xl font-semibold mb-4">Register</h1>
+                <p className="text-gray-600 mb-6">Enter your information</p>
+                <div className="mb-5">
+                  <input
+                    type="password"
+                    placeholder="Enter your password"
+                    className="email-input w-full px-4 py-2 border rounded-lg text-gray-700 focus:border-blue-500"
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className="mb-5">
+                  <input
+                    type="password"
+                    placeholder="Re-enter your password"
+                    className="email-input w-full px-4 py-2 border rounded-lg text-gray-700 focus:border-blue-500"
+                    onChange={(e) => {
+                      setConfirmPassword(e.target.value);
+                    }}
+                  />
+                </div>
+
+                <button
+                  className="w-full bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 focus:outline-none"
+                  onClick={(e) => {
+                    handleSignUp(e);
+                  }}
+                >
+                  Submit
+                </button>
+              </div>
+            </motion.div>
+          </AnimatePresence>
+        )}
       </div>
     </>
   );
