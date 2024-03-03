0bb648697382102d58603e1505115becf2232563
leminhduy1912 <minhduyle081003@gmail.com>
2024-03-03 21:00:34 +0700

    feat:intergrate api of seat ordered

--- src/components/Seat.jsx
+++ src/components/Seat.jsx
@@ -1,7 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../components/Seat/style.css";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const Seat = () => {
+  let { showTimeId } = useParams();
+  showTimeId = showTimeId.replace("showTimeId=", "");
+
+  const [listSeatOrdered, setListSeatOrdered] = useState([]);
+
+  // Trong useEffect, thay vì thêm các phần tử vào mảng trực tiếp, bạn nên sử dụng setListSeatOrdered để cập nhật giá trị mới:
+  useEffect(() => {
+    axios
+      .get(
+        `${import.meta.env.VITE_API_BE_KEY}/api/v1/booking?showTimeId=` +
+          showTimeId
+      )
+      .then((response) => {
+        console.log(response.data.data);
+        const updatedList = response.data.data.result.map((item) => ({
+          row: item.seatName.substring(0, 1),
+          seat: item.seatName.substring(1),
+          status: item.status,
+        }));
+        setListSeatOrdered(updatedList);
+      })
+      .catch((error) => {
+        // Xử lý lỗi
+        console.error("Error:", error);
+      });
+  }, [showTimeId]);
+
+  console.log(listSeatOrdered);
   const rows = ["A", "B", "C", "D", "E", "F"];
   const nums = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
@@ -16,15 +46,14 @@ const Seat = () => {
     console.log("row", row);
     console.log("seat", seat);
     const isSelected = selectedSeats.some(
-      (selectedSeat) => selectedSeat.row === row && selectedSeat.seat === seat
+      (selectedSeat) => selectedSeat.row == row && selectedSeat.seat == seat
     );
     console.log("is selected ?", isSelected);
     console.log(selectedSeats);
 
     if (isSelected) {
-      // Nếu ghế đã được chọn thì loại bỏ khỏi danh sách các ghế đã chọn
       setSelectedSeats(
-        selectedSeats.filter((s) => !(s.row === row && s.seat === seat))
+        selectedSeats.filter((s) => !(s.row == row && s.seat == seat))
       );
     } else {
       console.log("chua duoc chon");
@@ -32,52 +61,70 @@ const Seat = () => {
       setSelectedSeats([...selectedSeats, { row, seat }]);
     }
   };
-  console.log("ssssssssssssssssssssssssssssss", selectedSeats);
+  console.log("selected seat", selectedSeats);
+  console.log("ordered seat", listSeatOrdered);
   return (
-    <div className="mt-10 flex flex-col justify-center items-center w-[700px] h-[450px] gap-20">
-      <div className="screen">Screen</div>
-      <div className="w-full flex flex-col justify-start items-center gap-1">
-        {rows.map((row) => (
-          <div
-            key={row}
-            className="flex justify-start items-center w-[98%] h-[50px]"
-          >
-            <div className="min-w-[165px]">
-              <span className=" font-semibold text-3xl text-white max-w-10">
-                {row}
-              </span>
-            </div>
+    <>
+      {listSeatOrdered.length > 0 && (
+        <div className="mt-10 flex flex-col justify-center items-center w-[700px] h-[450px] gap-20">
+          <div className="screen">Screen</div>
+          <div className="w-full flex flex-col justify-start items-center gap-1">
+            {rows.map((row) => (
+              <div
+                key={row}
+                className="flex justify-start items-center w-[98%] h-[50px]"
+              >
+                <div className="min-w-[165px]">
+                  <span className=" font-semibold text-3xl text-white max-w-10">
+                    {row}
+                  </span>
+                </div>
 
-            <div className="flex">
-              {seats.map((seat) => (
-                <div
-                  key={seat}
-                  className={`min-h-[30px] cursor-pointer min-w-[35px] m-[5px] bg-${
-                    selectedSeats.some(
+                <div className="flex">
+                  {seats.map((seat) => {
+                    const isOrdered = listSeatOrdered.some(
+                      (orderedSeat) =>
+                        orderedSeat.row == row && orderedSeat.seat == seat
+                    );
+                    console.log(isOrdered);
+                    const isSelected = selectedSeats.some(
                       (selectedSeat) =>
-                        selectedSeat.row === row && selectedSeat.seat === seat
-                    )
-                      ? "white"
-                      : "slate-800"
-                  } rounded-t-2xl`}
-                  onClick={() => toggleSeat(row, seat)} // Gọi hàm xử lý khi click vào ghế
-                ></div>
+                        selectedSeat.row == row && selectedSeat.seat == seat
+                    );
+
+                    return (
+                      <div
+                        key={seat}
+                        className={`min-h-[30px] cursor-pointer min-w-[35px] m-[5px] ${
+                          isOrdered
+                            ? "bg-white pointer-events-none"
+                            : isSelected
+                            ? "bg-green-500"
+                            : "bg-slate-800"
+                        } rounded-t-2xl`}
+                        onClick={() => toggleSeat(row, seat)}
+                      ></div>
+                    );
+                  })}
+                </div>
+              </div>
+            ))}
+            <div className="w-[52%] flex justify-start items-center mt-10 ">
+              {nums.map((num, index) => (
+                <div
+                  key={index}
+                  className="w-12 h-10 flex justify-center items-center mx-4"
+                >
+                  <span className="text-xl font-semibold text-white">
+                    {num}
+                  </span>
+                </div>
               ))}
             </div>
           </div>
-        ))}
-        <div className="w-[52%] flex justify-start items-center mt-10 ">
-          {nums.map((num, index) => (
-            <div
-              key={index}
-              className="w-12 h-10 flex justify-center items-center mx-4"
-            >
-              <span className="text-xl font-semibold text-white">{num}</span>
-            </div>
-          ))}
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
